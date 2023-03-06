ad030a41ff1a06d455ce8a25f3fa9e239481398d
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-03-06 14:29:53 -0300

    Initial commit

---
+++ src/Elevator.java
@@ -0,0 +1,117 @@
+public class Elevator {
+
+    private int currentFloor = 0;
+    private int totalFloors;
+
+    private int capacity;
+    private int amountPerson = 0;
+
+    public Elevator() {
+
+    }
+
+    public Elevator(int totalFloors, int capacity) {
+        this.currentFloor = 0;
+        this.totalFloors = totalFloors;
+        this.capacity = capacity;
+        this.amountPerson = 0;
+    }
+
+    public Elevator(int currentFloor, int totalFloors, int capacity, int amountPerson) {
+        this.currentFloor = currentFloor;
+        this.totalFloors = totalFloors;
+        this.capacity = capacity;
+        this.amountPerson = amountPerson;
+    }
+
+
+    public void initialize(double maxCapacity, double totalFloors) {
+        this.capacity += maxCapacity;
+        this.totalFloors += totalFloors;
+        System.out.println();
+
+    }
+
+    public void enter(int increasePerson) {
+
+        if (capacity > amountPerson && increasePerson <= (capacity - amountPerson)) {
+            this.amountPerson += increasePerson;
+
+        } else {
+            this.amountPerson = capacity;
+            System.out.println("The elevator is full");
+        }
+    }
+
+    public void out(int decreasePerson) {
+        if (amountPerson > 0 && decreasePerson <= amountPerson) {
+            this.amountPerson -= decreasePerson;
+        } else {
+            amountPerson = 0;
+        }
+
+    }
+
+    public boolean up(int choose) {
+        if (currentFloor >= 0 && currentFloor <= totalFloors && choose <= totalFloors && choose > currentFloor) {
+            this.currentFloor = choose;
+            return true;
+        } else {
+            System.out.println("This floor doesn't exist");
+            return false;
+        }
+
+    }
+
+    public boolean down(int choose) {
+        if (currentFloor > 0 && choose < currentFloor) {
+            this.currentFloor = choose;
+            return true;
+        } else {
+            System.out.println("This floor doesn't exist");
+            return false;
+        }
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void setCurrentFloor(int currentFloor) {
+        this.currentFloor = currentFloor;
+    }
+
+    public int getTotalFloors() {
+        return totalFloors;
+    }
+
+    public void setTotalFloors(int totalFloors) {
+        this.totalFloors = totalFloors;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getAmountPerson() {
+        return amountPerson;
+    }
+
+    public void setAmountPerson(int amountPerson) {
+        this.amountPerson = amountPerson;
+    }
+
+    @Override
+    public String toString() {
+        return "Welcome, " +
+                "your current floor is " + currentFloor +
+                ", the highest floor is " + totalFloors +
+                ", max person capacity in elevator is: " + capacity +
+                ", current person in elevator: " + amountPerson;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int choose = 0;
+        boolean up, down, check = true;
+        Elevator ele = new Elevator();
+
+        System.out.println("Configure your elevator with max capacity and total floor");
+        ele.initialize(20, 10);
+        System.out.println(ele);
+
+        do {
+            System.out.println("How much person will enter");
+            ele.enter(sc.nextInt());
+
+            do {
+                System.out.println("1- Up  or 2- Down");
+                choose = sc.nextInt();
+                switch (choose) {
+
+                    case 1:
+
+                        while (check) {
+                            System.out.println("Which floor did you wanna go up?");
+                            up = ele.up(sc.nextInt());
+
+                            if (up) {
+                                System.out.println("How much person will exit?");
+                                ele.out(sc.nextInt());
+                                check = false;
+                            }
+                        }
+
+                        System.out.println(ele);
+
+                        break;
+                    case 2:
+
+                        while (check) {
+                            System.out.println("Which floor did you wanna go down?");
+                            down = ele.down(sc.nextInt());
+                            if (down) {
+                                System.out.println("How much person will exit?");
+                                ele.out(sc.nextInt());
+                                check = false;
+                            }
+                        }
+                        System.out.println(ele);
+                        break;
+                    default:
+                        System.out.println("Wrong choice. ");
+                        System.out.println(ele);
+                }
+
+            } while (choose != 1 && choose != 2);
+
+
+        } while (ele.getAmountPerson() != 0);
+
+
+        sc.close();
+
+    }
+
+}
